b6e3b13a2dbafbe6f4ebfd69fd736bb9f31853a5
Cyncrovee <ellasacc5682@gmail.com>
2024-12-03 23:51:13 +0000

    Changed main.py to use questionary

--- cynvee_hash_checker/main.py
+++ cynvee_hash_checker/main.py
@@ -4,30 +4,35 @@
 import sys
 from file_config import file_config
 from path_config import path_config
+import questionary as qy
 
 
 def main():
     """
     Main function to handle user input for file or path configuration.
 
-    Prompts the user to input either 'n' for file configuration, 'p'
+    Prompts the user to input either 'Name' for file configuration, 'Path'
     for path configuration,
-    or 'ex'/'exit' to exit the program. Based on the input,
+    or 'Exit' to exit the program. Based on the input,
     it calls the appropriate function
     or exits the program.
 
     Raises:
         SystemExit: If the user chooses to exit the program.
     """
-    user_config = input(
-        "File name or path? (n/p, case insensitive."
-        "You can also type [ex]it to exit the program) "
-        ).lower()
-    if user_config == "n":
+    user_config = qy.select(
+        "File name or path? (or exit program)",
+        choices=[
+            "Name",
+            "Path",
+            "Exit"
+        ]
+    ).ask()
+    if user_config == "Name":
         file_config()
-    elif user_config == "p":
+    elif user_config == "Path":
         path_config()
-    elif user_config == "ex" or user_config == "exit":
+    elif user_config == "Exit":
         print("Exiting Program...")
         sys.exit(0)
     else:
