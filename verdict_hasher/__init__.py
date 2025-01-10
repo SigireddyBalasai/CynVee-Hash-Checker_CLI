b64ae503c035b5c0d8f2b1e4c8a6df42d0070416
Cyncrovee <86919717+Cyncrovee@users.noreply.github.com>
2025-01-10 23:47:02 +0000

    Remove script entry point

--- verdict_hasher/__init__.py
+++ verdict_hasher/__init__.py
@@ -2,4 +2,4 @@
 This module initializes the verdict_hasher package.
 """
 
-__all__ = ["file_config", "hash_file", "main", "path_config", "__main__"]
+__all__ = ["file_config", "hash_file", "main", "path_config"]
